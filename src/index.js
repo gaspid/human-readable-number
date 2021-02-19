808bcef12dd2887472908e853717316da0a1029d
gaspid <gaspid194@gmail.com>
2021-02-19 19:28:16 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,32 +1,34 @@
   
 module.exports = function toReadable (number) {
-    let digits1 = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    let digits2 = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen' , 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    let digits3 = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-
-    let str = '';
-    let num = 0;
-    if (number === 0) str+= 'zero';
-    if (number >= 100) {
-        num = Math.floor(number / 100);
-        number = number % 100;
-        str += digits1[num - 1] + ' hundred';
+    if(number === 0) return 'zero';
+  
+    let sl = {
+      1:'one', 2:'two', 3:'three',  4:'four', 5:'five', 6:'six', 7:'seven', 8:'eight', 9:'nine',
+      10:'ten', 11:'eleven', 12:'twelve', 13:'thirteen', 14:'fourteen', 15:'fifteen', 16:'sixteen', 17:'seventeen', 18:'eighteen', 19:'nineteen',
+      20:'twenty', 30:'thirty', 40:'forty', 50:'fifty', 60:'sixty', 70:'seventy', 80:'eighty', 90:'ninety'
     }
-    if (number >= 20 && number <= 99) {
-        num = Math.floor(number / 10);
-        number = number % 10;
-        str += str.length != 0 ? ' ' + digits3[num - 1] : digits3[num - 1];
+  
+    let numWord = number.toString();
+  
+    let result = '';
+    let hundreds = '';
+  
+    if(numWord.length === 3) {
+      hundreds = sl[numWord[0]] + ' hundred';
+      numWord = numWord.slice(1);
     }
-    if (number > 10 && number <= 19) {
-        num = number % 10;
-        str += str.length != 0 ? ' ' + digits2[num - 1] : digits2[num - 1];
+  
+    if(sl[numWord]) {
+      result = hundreds + ' ' + sl[numWord];
+    } else {
+      if (numWord == '00') {
+        return hundreds;
+      }
+      let tens = sl[numWord[0]] ? sl[numWord[0]*10] : '';
+      let unit = sl[numWord[1]];
+      
+      result = (tens != '') ? (hundreds + ' ' + tens + ' ' + unit) : (hundreds + ' ' + unit);
     }
-    if (number === 10)
-    {
-        number = number % 10;
-        str += str.length != 0 ? ' ' + digits3[0] : digits3[0];
-    }
-    if (number < 10 && number > 0)
-        str += str.length != 0 ? ' ' + digits1[number - 1] : digits1[number - 1];
-    return str;
-}
+  
+    return result.trim();
+  }
